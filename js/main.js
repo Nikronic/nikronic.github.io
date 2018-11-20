f08e190ac0dea37da22664269e3277e5413f9ba4
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-11-21 01:32:55 +0330

    change offfuckingset

--- js/main.js
+++ js/main.js
@@ -187,7 +187,7 @@
                 navActive($(this.element).data('section'));
             }
         }, {
-            offset: '50px'
+            offset: '100px'
         });
 
         $section.waypoint(function (direction) {
