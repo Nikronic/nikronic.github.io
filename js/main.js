3abd80b84c38113fff14fcbad9dfb9611226234a
M. Doosti Lakhani <nikan.doosti@outlook.com>
2021-11-10 04:54:45 +0330

    Fix #14 and navbar animation

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@
 
             if ($('[data-section="' + section + '"]').length) {
                 $('html, body').animate({
-                    scrollTop: $('[data-section="' + section + '"]').offset().top - 55
+                    scrollTop: $('[data-section="' + section + '"]').offset().top - 40
                 }, 500);
             }
 
